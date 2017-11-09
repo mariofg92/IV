aa005a51a8bd0e624e3cb1855198b4be3b242431
mario <mariofg92@gmail.com>
2017-11-09 08:36:03 +0100

    cambios web.py

--- web.py
+++ web.py
@@ -1,18 +1,11 @@
-from flask import Flask
 import os
-ON_HEROKU = os.environ.get('ON_HEROKU')
-
-if ON_HEROKU:
-    # get the heroku port
-    new_port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-else:
-    new_port = 5000
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-   return "OK"
+@app.route("/")
+def hello():
+    return "OK!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=new_port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

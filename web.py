7bafc5745c88d17e5b13d26062c6a24d5832d818
mario <mariofg92@gmail.com>
2017-11-09 19:40:01 +0100

    add funcionalidad

--- web.py
+++ web.py
@@ -2,11 +2,14 @@ import hug
 import json
 from bot.funcionalidades import FuncionalidadDatos
 
+datos = FuncionalidadDatos()
+
 @hug.get('/')
 def status():
     return { "status": "OK" }
 
 @hug.get('/actividad')
 def all():
+    actividad = datos.ConsultarActividad()
     """Devuelve toda la actividad"""
-    return { "actividad": ConsultarActividad() }
+    return { "actividad": actividad }

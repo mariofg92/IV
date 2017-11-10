e48db200be2e053eaa1ffb050a51659d842cd592
mario <mariofg92@gmail.com>
2017-11-10 13:37:07 +0100

    solve pathnames

--- bot/funcionalidades.py
+++ bot/funcionalidades.py
@@ -7,8 +7,12 @@ import os
 class FuncionalidadDatos:
 
     def __init__(self):
+      if os.getcwd() is not os.path.dirname(os.path.abspath(__file__)):
+          path = 'bot/prueba_actividad.json'
+      else:
+          path = '/prueba_actividad.json'
       try: # De https://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file
-        with open('bot/prueba_actividad.json') as data_file:
+        with open(path) as data_file:
             self.actividad = json.load(data_file)
       except IOError as fallo:
         print("Error %d leyendo prueba_actividad.json: %s", fallo.errno,fallo.strerror)

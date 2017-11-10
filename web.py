1a2414f31c028a2c4d5ad4ee7c6a3b7a808f0aac
mario <mariofg92@gmail.com>
2017-11-10 13:20:48 +0100

    solve pathnames

--- web.py
+++ web.py
@@ -3,15 +3,15 @@ import json
 from bot import funcionalidades
 
 
-#datos = FuncionalidadDatos()
+datos = funcionalidades.FuncionalidadDatos()
 
 ##print os.name
 @hug.get('/')
 def status():
     return { "status": "OK" }
 
-'''@hug.get('/actividad')
+@hug.get('/actividad')
 def all():
     actividad = datos.ConsultarActividad()
     """Devuelve toda la actividad"""
-    return { "actividad": actividad }'''
+    return { "actividad": actividad }
